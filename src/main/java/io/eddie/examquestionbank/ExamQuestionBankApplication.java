c92781c68f1458f676870aab37b5268c5d5cac02
DESKTOP-QT9VDHF\Admin <oxju5382@gmail.com>
2026-05-14 09:17:57 +0900

    [Chore] 초기파일 구성

---
+++ src/main/java/io/eddie/examquestionbank/ExamQuestionBankApplication.java
@@ -0,0 +1,13 @@
+package io.eddie.examquestionbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamQuestionBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/eddie/examquestionbank/ExamQuestionBankApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eddie.examquestionbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
